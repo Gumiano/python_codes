8abff6b4c89b28da76422d8a2f4c01e768dc9f30
Gumiano <578168956@qq.com>
2022-06-08 09:31:29 +0800

    2022/6/8 9:31 最初的正常运行项目

--- code/人名分类/train_models.py
+++ code/人名分类/train_models.py
@@ -11,44 +11,45 @@ output_size = n_categories
 criterion = nn.CrossEntropyLoss()
 
 # 训练参数
-lr = 0.001 # 学习率
+lr = 0.001  # 学习率
 n_epochs = 100000  # 训练100000次（可重复的从数据集中抽取100000姓名）
-print_every = 1000 #每训练5000次，打印一次
-plot_every = 1000  #每训练1000次，计算一次训练平均误差
+print_every = 1000  # 每训练5000次，打印一次
+plot_every = 1000  # 每训练1000次，计算一次训练平均误差
+
 
 def rnn_step_one_epoch(rnn, optimizer, category_tensor, name_tensor, clip=False):
-        rnn.zero_grad()  # 将rnn网络梯度清零
-        hidden = rnn.initHidden()  # 只对姓名的第一字母构建起hidden参数
+    rnn.zero_grad()  # 将rnn网络梯度清零
+    hidden = rnn.initHidden()  # 只对姓名的第一字母构建起hidden参数
 
-        # 对姓名的每一个字母逐次学习规律。每次循环的得到的hidden参数传入下次rnn网络中
-        for i in range(name_tensor.size()[0]):
-            output, hidden = rnn(name_tensor[i], hidden)
+    # 对姓名的每一个字母逐次学习规律。每次循环的得到的hidden参数传入下次rnn网络中
+    for i in range(name_tensor.size()[0]):
+        output, hidden = rnn(name_tensor[i], hidden)
 
-        # 比较最终输出结果与 该姓名真实所属语言，计算训练误差
-        loss = criterion(output.squeeze(0), category_tensor)
+    # 比较最终输出结果与 该姓名真实所属语言，计算训练误差
+    loss = criterion(output.squeeze(0), category_tensor)
 
-        # 将比较后的结果反向传播给整个网络
-        loss.backward()
+    # 将比较后的结果反向传播给整个网络
+    loss.backward()
 
-        # 梯度裁剪, theta=1
-        if clip:
-            grad_clipping(rnn, 1)
+    # 梯度裁剪, theta=1
+    if clip:
+        grad_clipping(rnn, 1)
 
-        # 调整网络参数。
-        optimizer.step()
+    # 调整网络参数。
+    optimizer.step()
 
-        # 返回预测结果  和 训练误差
-        return output, loss.item()
+    # 返回预测结果  和 训练误差
+    return output, loss.item()
 
 
 def train_rnn(is_clip=False):
     rnn = RNN(input_size, n_hidden, output_size)
     optimizer = torch.optim.Adam(params=rnn.parameters(), lr=lr)
     # 误差
-    current_loss = 0 # 初始误差为0
-    all_losses = [] # 记录平均误差
+    current_loss = 0  # 初始误差为0
+    all_losses = []  # 记录平均误差
 
-    #训练开始时间点
+    # 训练开始时间点
     start = time.time()
 
     for epoch in range(1, n_epochs + 1):
@@ -57,11 +58,12 @@ def train_rnn(is_clip=False):
         output, loss = rnn_step_one_epoch(rnn, optimizer, category_tensor, name_tensor, clip=is_clip)
         current_loss += loss
 
-        #每训练5000次，预测一个姓名，并打印预测情况
+        # 每训练5000次，预测一个姓名，并打印预测情况
         if epoch % print_every == 0:
             guess, guess_i = categoryFromOutput(output)
             correct = '✓' if guess == category else '✗ (%s)' % category
-            print('%d %d%% (%s) %.4f %s / %s %s' % (epoch, epoch / n_epochs * 100, time_since(start), loss, name, guess, correct))
+            print('%d %d%% (%s) %.4f %s / %s %s' % (
+            epoch, epoch / n_epochs * 100, time_since(start), loss, name, guess, correct))
 
         # 每训练5000次，计算一个训练平均误差，方便后面可视化误差曲线图
         if epoch % plot_every == 0:
@@ -75,6 +77,7 @@ def train_rnn(is_clip=False):
         torch.save(rnn.state_dict(), './model/model_rnn_no_clip.pth')
 
     return all_losses, int(time.time() - start)
+
 
 def lstm_step_one_epoch(lstm: LSTM, optimizer, category_tensor, name_tensor, clip=False):
     lstm.zero_grad()  # 将rnn网络梯度清零
@@ -99,6 +102,7 @@ def lstm_step_one_epoch(lstm: LSTM, optimizer, category_tensor, name_tensor, cli
 
     # 返回预测结果  和 训练误差
     return output, loss.item()
+
 
 def gru_step_one_epoch(gru, optimizer, category_tensor, name_tensor, clip=False):
     gru.zero_grad()  # 将rnn网络梯度清零
@@ -129,10 +133,10 @@ def train_lstm(is_clip=False):
     lstm = LSTM(input_size, n_hidden, output_size)
     optimizer = torch.optim.Adam(params=lstm.parameters(), lr=lr)
     # 误差
-    current_loss = 0 # 初始误差为0
-    all_losses = [] # 记录平均误差
+    current_loss = 0  # 初始误差为0
+    all_losses = []  # 记录平均误差
 
-    #训练开始时间点
+    # 训练开始时间点
     start = time.time()
 
     for epoch in range(1, n_epochs + 1):
@@ -141,11 +145,12 @@ def train_lstm(is_clip=False):
         output, loss = lstm_step_one_epoch(lstm, optimizer, category_tensor, name_tensor, clip=is_clip)
         current_loss += loss
 
-        #每训练5000次，预测一个姓名，并打印预测情况
+        # 每训练5000次，预测一个姓名，并打印预测情况
         if epoch % print_every == 0:
             guess, guess_i = categoryFromOutput(output)
             correct = '✓' if guess == category else '✗ (%s)' % category
-            print('%d %d%% (%s) %.4f %s / %s %s' % (epoch, epoch / n_epochs * 100, time_since(start), loss, name, guess, correct))
+            print('%d %d%% (%s) %.4f %s / %s %s' % (
+            epoch, epoch / n_epochs * 100, time_since(start), loss, name, guess, correct))
 
         # 每训练5000次，计算一个训练平均误差，方便后面可视化误差曲线图
         if epoch % plot_every == 0:
@@ -159,14 +164,15 @@ def train_lstm(is_clip=False):
         torch.save(lstm.state_dict(), './model/model_lstm_no_clip.pth')
     return all_losses, int(time.time() - start)
 
+
 def train_gru(is_clip=False):
     gru = GRU(input_size, n_hidden, output_size)
     optimizer = torch.optim.Adam(params=gru.parameters(), lr=lr)
     # 误差
-    current_loss = 0 # 初始误差为0
-    all_losses = [] # 记录平均误差
+    current_loss = 0  # 初始误差为0
+    all_losses = []  # 记录平均误差
 
-    #训练开始时间点
+    # 训练开始时间点
     start = time.time()
 
     for epoch in range(1, n_epochs + 1):
@@ -175,11 +181,12 @@ def train_gru(is_clip=False):
         output, loss = gru_step_one_epoch(gru, optimizer, category_tensor, name_tensor, clip=is_clip)
         current_loss += loss
 
-        #每训练5000次，预测一个姓名，并打印预测情况
+        # 每训练5000次，预测一个姓名，并打印预测情况
         if epoch % print_every == 0:
             guess, guess_i = categoryFromOutput(output)
             correct = '✓' if guess == category else '✗ (%s)' % category
-            print('%d %d%% (%s) %.4f %s / %s %s' % (epoch, epoch / n_epochs * 100, time_since(start), loss, name, guess, correct))
+            print('%d %d%% (%s) %.4f %s / %s %s' % (
+            epoch, epoch / n_epochs * 100, time_since(start), loss, name, guess, correct))
 
         # 每训练5000次，计算一个训练平均误差，方便后面可视化误差曲线图
         if epoch % plot_every == 0:
--- code/人名分类/utils.py
+++ code/人名分类/utils.py
@@ -2,21 +2,30 @@ import random
 import torch
 import time
 import math
-
 import torch.nn as nn
+
 from preprocessing import all_categories, category_lines, lineToTensor
 
 def categoryFromOutput(output):
-    """从输出结果中获得指定类别，参数为输出张量output"""
+    """获取输出中概率最大的类别
+
+    从模型输出张量中获取top 1个值和索引，并根据索引查找对应类别，
+
+    Returns:
+        (概率最大的类别, 该类别在all_categories类别向量表中的索引)，例如：
+
+        (French, 5)
+    """
     # 从输出张量中返回最大的值和索引对象
     top_n, top_i = output.topk(1)
     # 从top_i对象中取出索引的值
     category_i = top_i[0].item()
     # 根据索引值获取对应语言类别，返回语言类别和索引值
+    print('(all_categories[category_i], category_i):', all_categories[category_i], category_i)
     return all_categories[category_i], category_i
 
 def randomTrainingExample():
-    """用于随机产生训练数据"""
+    """返回随机抽取的训练样本。"""
     # 首先使用random.choice随机选取一个类别
     category = random.choice(all_categories)
     # 然后通过category_lines字典获取category类别对应的名字列表，并随机选取一个名字
@@ -28,9 +37,10 @@ def randomTrainingExample():
     return category, line, category_tensor, line_tensor
 
 def grad_clipping(net, theta):
-    """
-    梯度裁剪
-    g <- min (1, theta/||g||) * g
+    """对刚刚更新的梯度进行梯度裁剪。
+
+    公式为：g <- min (1, theta/||g||) * g
+    该函数应该放在损失函数backward()之后，优化器step()之前
     """
     if isinstance(net, nn.Module):
         params = [p for p in net.parameters() if p.requires_grad]
